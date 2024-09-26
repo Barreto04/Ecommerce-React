47eae9f8966884c2b1837bd5d7ea12afaa3d18a9
Barreto04 <joao.vmbarreto@gmail.com>
2024-09-26 17:57:34 -0300

    Primeiro commit do projeto

---
+++ E-commerceR/src/components/utils/catalog.js
@@ -0,0 +1,75 @@
+import product1Img from '../assets/img/product-1.jpg';
+import product2Img from '../assets/img/product-2.jpg';
+import product3Img from '../assets/img/product-3.jpg';
+import product4Img from '../assets/img/product-4.jpg';
+import product5Img from '../assets/img/product-5.jpg';
+import product6Img from '../assets/img/product-6.jpg';
+import product7Img from '../assets/img/product-7.jpg';
+import product8Img from '../assets/img/product-8.jpg';
+
+export const catalog = [
+  {
+    id: 1,
+    brand: 'São-Paulo',
+    name: 'camiseta branca (Uniforme 1)',
+    price: 70,
+    image: product1Img,
+    Europeu: false,
+  },
+  {
+    id: 2,
+    brand: 'Real Madrid',
+    name: 'camiseta branca (Uniforme 1)',
+    price: 85,
+    image: product2Img,
+    Europeu: true,
+  },
+  {
+    id: 3,
+    brand: 'Palmeiras',
+    name: 'camiseta verde (Uniforme 1)',
+    price: 60,
+    image: product3Img,
+    Europeu: false,
+  },
+  {
+    id: 4,
+    brand: 'Seleção Brasileira',
+    name: 'camiseta Amarela (Uniforme 1)',
+    price: 160,
+    image: product4Img,
+    Europeu: false,
+  },
+  {
+    id: 5,
+    brand: 'Internacional',
+    name: 'camiseta vermelha (Uniforme 1)',
+    price: 110,
+    image: product5Img,
+    Europeu: false,
+  },
+  {
+    id: 6,
+    brand: 'Inter de Milão',
+    name: 'camiseta azul/preto (Uniforme 1)',
+    price: 170,
+    image: product6Img,
+    Europeu: true,
+  },
+  {
+    id: 7,
+    brand: 'Manchester city',
+    name: 'camiseta Azul (Uniforme 1)',
+    price: 75,
+    image: product7Img,
+    Europeu: true,
+  },
+  {
+    id: 8,
+    brand: 'Bayern de munich',
+    name: 'camiseta vermelha (Uniforme 1)',
+    price: 88,
+    image: product8Img,
+    Europeu: true,
+  },
+];
